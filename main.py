85210138f7b901623c19f15d746e548cdcb1b19d
Kanakaraja <kanaka_raja@yahoo.com>
2025-02-16 13:08:56 +0530

    va

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def load_page():
     df = pd.DataFrame(data)
 
     # Display the DataFrame
-    print("Original DataFrame test:\n", df)
+    print("Original DataFrame test123:\n", df)
 
     # Get information about the DataFrame
     print("\nDataFrame Info:\n", df.info())
